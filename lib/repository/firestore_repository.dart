3a7d790612d6df606e5a16762c17045a2ea6cd57
Ludwig B <bolling.ludwig@gmail.com>
2023-04-15 10:11:40 +0200

    If Sofort payment launch payment flow in external browser.

--- lib/repository/firestore_repository.dart
+++ lib/repository/firestore_repository.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-
 import '../model/chat.dart';
 import '../model/chat_user.dart';
 import '../utils/log.dart';
